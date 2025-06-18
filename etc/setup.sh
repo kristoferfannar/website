b8b7ce26590fa768cdea72b5aa3f36cefd8cb17f
Kristofer <kristoferfannarb@gmail.com>
2025-06-18 03:50:06 +0000

    fix setup.sh script

--- etc/setup.sh
+++ etc/setup.sh
@@ -1,8 +1,8 @@
-!#/bin/bash
+#!/bin/bash
 
 # This script is intended to run inside an ec2 instance
 # don't think there's a better way to check this
-if [[ "$USER" != "ec2-user"]]; then
+if [[ "$USER" != "ec2-user" ]]; then
 	echo "error: This script should run in an ec2-instance"
 	exit 1
 fi
@@ -11,6 +11,6 @@ fi
 # - ensure docker (and docker-compose) is installed 
 # - ensure nginx is installed
 
-sudo mv ./nginx/ /etc/nginx
+sudo rm -rf /etc/nginx && sudo cp -R ./etc/nginx/ /etc/nginx
 docker-compose up --build -d
 sudo nginx -t && sudo systemctl restart nginx
